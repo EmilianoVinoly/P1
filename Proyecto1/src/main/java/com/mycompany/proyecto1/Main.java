8d5d8aaf127fa0793cfc8f8ac0b4b92c7b11b733
EmilianoVinoly <67710837+EmilianoVinoly@users.noreply.github.com>
2020-07-01 16:51:28 -0300

    Add files via upload

---
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Main.java
@@ -0,0 +1,22 @@
+package com.mycompany.proyecto1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        String nombre = "Emiliano";
+        int edad = 15; 
+        System.out.println("Mi nombre es " + nombre); 
+        System.out.println ("y tengo " + edad);
+    
+    }
+}
